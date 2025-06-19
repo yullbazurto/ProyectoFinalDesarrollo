af68a9931609a5ce9c5245f657e4914b6386da73
Lautaro-Arreaga <lautaro.arreaga@uees.edu.ec>
2025-06-19 13:53:49 -0500

    componente acerca

---
+++ src/app/pages/acerca/acerca.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acerca',
+  imports: [],
+  templateUrl: './acerca.component.html',
+  styleUrl: './acerca.component.css'
+})
+export class AcercaComponent {
+
+}
