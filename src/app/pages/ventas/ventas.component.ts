95e7bea3772c0351cf4913fec5c22e2a36297e22
Yull Bazurto <ybazurto@uees.edu.ec>
2025-06-19 13:55:55 -0500

    Creacion de componente pages/ventas

---
+++ src/app/pages/ventas/ventas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ventas',
+  imports: [],
+  templateUrl: './ventas.component.html',
+  styleUrl: './ventas.component.css'
+})
+export class VentasComponent {
+
+}
