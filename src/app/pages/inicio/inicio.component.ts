6fc296516a3fb0ac3f189a7baefa42acde20d551
Niurlyyy <gorozabelniurly@gmail.com>
2025-06-19 14:08:57 -0500

    Componentes de inicio

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // <-- IMPORTANTE
+import { HomeCarouselComponent } from '../../components/home-carousel/home-carousel.component';
+
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule, HomeCarouselComponent], // <-- AGREGA AQUÍ RouterModule
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
-
-}
+export class InicioComponent {}
